6cd58ea96165b2ba963151e568d2466af21855a0
tbrunk <111793207+tbrunk@users.noreply.github.com>
2022-11-29 20:32:56 -0500

    Add files via upload

---
+++ MiniProjectPath2.py
@@ -0,0 +1,22 @@
+import pandas
+'''
+ The following is the starting code for path2 for data reading to make your first step easier.
+ 'dataset_2' is the clean data for path2.
+'''
+
+with open('behavior-performance.txt','r') as f:
+    raw_data = [x.strip().split('\t') for x in f.readlines()]
+df = pandas.DataFrame.from_records(raw_data[1:],columns=raw_data[0])
+df['VidID']       = pandas.to_numeric(df['VidID'])
+df['fracSpent']   = pandas.to_numeric(df['fracSpent'])
+df['fracComp']    = pandas.to_numeric(df['fracComp'])
+df['fracPlayed']  = pandas.to_numeric(df['fracPlayed'])
+df['fracPaused']  = pandas.to_numeric(df['fracPaused'])
+df['numPauses']   = pandas.to_numeric(df['numPauses'])
+df['avgPBR']      = pandas.to_numeric(df['avgPBR'])
+df['stdPBR']      = pandas.to_numeric(df['stdPBR'])
+df['numRWs']      = pandas.to_numeric(df['numRWs'])
+df['numFFs']      = pandas.to_numeric(df['numFFs'])
+df['s']           = pandas.to_numeric(df['s'])
+dataset_2 = df
+print(dataset_2[15620:25350].to_string()) #This line will print out the first 35 rows of your data
